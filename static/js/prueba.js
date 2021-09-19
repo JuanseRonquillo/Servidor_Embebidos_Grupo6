3aef9c4d6193370e445803653e0651dc735c526c
JuanseRonquillo <89862731+JuanseRonquillo@users.noreply.github.com>
2021-09-19 16:52:04 -0500

    Add files via upload

---
+++ static/js/prueba.js
@@ -0,0 +1,7 @@
+alert('Hola desde prueba');
+document. getElementById('sensor').innerHTML='Valor';
+function LED1_On(){document. getElementById('sensor').innerHTML='Encendido';
+	console.log('Se preciono el boton Encendido:');}
+
+function LED1_Off(){document. getElementById('sensor').innerHTML='Apagado';
+	console.log('Se preciono el boton Apagado:');}
